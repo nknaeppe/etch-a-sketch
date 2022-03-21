a6fc78cf27ef27e835eb97fd3fd3010cc4a472a8
nknaeppe <nilsknaepper@gmail.com>
2022-03-21 06:23:40 +0100

    variable  height and width added

--- index.js
+++ index.js
@@ -1,25 +1,64 @@
 const colorInput = document.querySelector("#color");
+const widthInput = document.querySelector("#xPixel");
+const heightInput = document.querySelector("#yPixel");
+const clearButton = document.querySelector("#clear");
 const sketchtGridContainer = document.querySelector(".sketch-grid");
-
+const newGrid = document.querySelector("#newGrid")
 function getInputColor() {
-    console.log(this.value);
-}
+    pickedColor = this.value;
 
-function fillGrid() {
-    for (let i = 0; i < 256; i++){
-        console.log(i);
-        // const suffix = "px"
+}
+function getInputWidth() {
+    gridWidth = this.value;
+    updateGrid();
+}
+function getInputHeight() {
+    gridHeight = this.value;    
+    updateGrid();
+}
+function clearGrid() {
+    sketchtGridContainer.innerHTML = "";
+}
+function updateGrid() {
+    clearGrid();
+    fillGrid(gridWidth * gridHeight);
+    coloring();
+}
+function fillGrid(number) {
+    for (let i = 0; i < number; i++){
         const gridItem = document.createElement("div");
-        // gridItem.style.width = calcGridBoxWidth() + suffix;
-        // gridItem.style.height = calcGridBoxHeight() + suffix;
+        const suffix = "%"
+        gridItem.style.height = `${calcGridItemSizeY(gridHeight)}` + suffix;
+        gridItem.style.width = `${calcGridItemSizeX(gridWidth)}` + suffix;
+        gridItem.id = i;
         gridItem.classList += "grid";
         sketchtGridContainer.appendChild(gridItem);       
     }
 }
+function calcGridItemSizeX(xPixel) {
+    return 100 / xPixel;
+}
+function calcGridItemSizeY(yPixel) {
+    return 100 / yPixel
+}
 
-
-fillGrid();
-updateGridBoxSize();
+function colorGrid(gridItem) {
+    const item = document.querySelector("#" + gridItem.id)
+    console.log(item)
+    item.style.backgroundColor = pickedColor;
+}
+function coloring() {
+    let wholeGrid = document.querySelectorAll(".grid");
+    wholeGrid.forEach((gridItem) => {gridItem.addEventListener("click", () => gridItem.style.backgroundColor=pickedColor)});
+}
+let pickedColor = "black";
+let gridWidth = 16;
+let gridHeight = 16;
+let wholeGrid = document.querySelectorAll(".grid");
+updateGrid();
+widthInput.addEventListener("input", getInputWidth);
+heightInput.addEventListener("input", getInputHeight);
 colorInput.addEventListener("change", getInputColor);
-//document.addEventListener("resize", updateGridBoxSize());
+newGrid.addEventListener("click", () => updateGrid())
+clearButton.addEventListener("click", () => clearGrid())
 
